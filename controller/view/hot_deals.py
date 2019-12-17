bd07a0ba4afd06911acd71e4eb10d5cf40c89aa0
Oliver <oliverdeng1005@hotmail.com>
2019-12-17 15:56:59 +1300

    update

--- controller/view/hot_deals.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-from data_config import *
-hot_deals_views = Blueprint('hot_deals', __name__)
-
-
-@hot_deals_views.route("/store/hotdeals")
-def hot_deals():
-
-    return render_template('pages/Product/hotdeals/index.html', **contact_info,**category_info,active_position="Hot Deals")
