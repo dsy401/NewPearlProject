8b6929e9abd3a6c3eeb24d82838f8e72a670ee98
Oliver <oliverdeng1005@hotmail.com>
2020-01-14 16:03:18 +1300

    update

--- controller/api/brand.py
+++ controller/api/brand.py
@@ -17,15 +17,17 @@ def get_brand_data():
     return res.convert_to_json()
 
 @brand_api.route('/api/brand',methods=["POST"])
+@auth.login_required
 def post_brand_data():
     form = request.form
+
     new_brand = brand(name=form['name'], name_cn=form['name_cn'],description=form['description'],description_cn=form['description_cn'],image='/static/home/img/brands/guanzhu.png')
 
     new_brand.save()
 
     all_brand = brand.objects()
     brand_list = []
-    for i in range(all_brand.all_brand()):
+    for i in range(all_brand.count()):
         brand_list.append(all_brand[i])
     res = result(True, brand_list, None)
     return res.convert_to_json()
