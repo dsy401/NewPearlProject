b1941af9b93b9acadeb60528e512db512764d1fa
Oliver <oliverdeng1005@hotmail.com>
2020-01-17 12:07:48 +1300

    updatye

--- controller/api/general.py
+++ controller/api/general.py
@@ -1,9 +1,12 @@
-# from flask import Blueprint,request
-# from model.result import result
-# from utils.auth import auth
-#
-#
-#
-# general_api = Blueprint('general_api', __name__)
-#
-#
+from flask import Blueprint,request
+from model.result import result
+from utils.auth import auth
+
+
+
+general_api = Blueprint('general_api', __name__)
+
+
+@general_api.route('/api/general/change_header_img',methods=['PUT'])
+def change_header_img():
+    return
