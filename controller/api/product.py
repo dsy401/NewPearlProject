4111daaa446b05349cd07f63497350f5dbbd71b6
Oliver <oliverdeng1005@hotmail.com>
2020-01-20 10:45:09 +1300

    update

--- controller/api/product.py
+++ controller/api/product.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint,request
 from model.result import result
 from bson import ObjectId
 from model.product import product
+from utils.auth import auth
 product_api = Blueprint('product_api', __name__)
 
 
@@ -32,3 +33,47 @@ def get_all_products():
         products_list.append(products[i])
     res = result(True, products_list, None)
     return res.convert_to_json()
+
+@product_api.route('/api/product',methods=['POST'])
+@auth.login_required
+def add_one_product():
+    form = request.form
+    new_product = product(
+        code=form['code'],image=form['image'],product_category_id=ObjectId(form['product_category_id']),color=form['color'],environment=form['environment'],
+        finish=form['finish'],material=form['material'],price=form['price'],shape=form['shape'],size=form['size'],style=form['style'],type=form['type'],unit=form['unit'],
+        color_cn=form['color_cn'],environment_cn=form['environment'],finish_cn=form['finish_cn'],material_cn=form['material_cn'],price_cn=form['price_cn'],shape_cn=form['shape_cn'],
+        size_cn=form['size_cn'],style_cn=form['style_cn'],type_cn=form['type_cn'],unit_cn=form['unit_cn']
+    )
+    new_product.save()
+
+    products = product.objects(product_category_id=ObjectId(form['product_category_id']))
+    products_list = []
+    for i in range(products.count()):
+        products_list.append(products[i])
+    res = result(True, products_list, None)
+    return res.convert_to_json()
+
+
+@product_api.route('/api/product/<id>',methods=['PUT'])
+@auth.login_required
+def update_one_product(id):
+    found_product = product.objects(id=ObjectId(id)).first()
+    if found_product != None:
+        form = request.form
+        found_product.update(
+        code=form['code'],image=form['image'],product_category_id=ObjectId(form['product_category_id']),color=form['color'],environment=form['environment'],
+        finish=form['finish'],material=form['material'],price=form['price'],shape=form['shape'],set__size__=form['size'],style=form['style'],set__type__=form['type'],unit=form['unit'],
+        color_cn=form['color_cn'],environment_cn=form['environment'],finish_cn=form['finish_cn'],material_cn=form['material_cn'],price_cn=form['price_cn'],shape_cn=form['shape_cn'],
+        size_cn=form['size_cn'],style_cn=form['style_cn'],type_cn=form['type_cn'],unit_cn=form['unit_cn']
+        )
+
+
+        products = product.objects(product_category_id=ObjectId(form['product_category_id']))
+        products_list = []
+        for i in range(products.count()):
+            products_list.append(products[i])
+        res = result(True, products_list, None)
+        return res.convert_to_json()
+    else:
+        res = result(False, None, "ID Error or People Not Found")
+        return res.convert_to_json(), 400
--- controller/api/staff.py
+++ controller/api/staff.py
@@ -34,3 +34,35 @@ def update_staff_data(id):
     else:
         res = result(False, None, "ID Error or People Not Found")
         return res.convert_to_json(), 400
+
+
+@staff_api.route('/api/staff',methods=['POST'])
+@auth.login_required
+def add_one_staff():
+    form = request.form
+    new_staff = staff(name=form['name'],role=form['role'],role_cn=form['role_cn'],facebook=form['facebook'],linkedin=form['linkedin'],wechat=form['wechat'],image=form['image'])
+    new_staff.save()
+
+    staffs = staff.objects()
+    staff_list = []
+    for i in range(staffs.count()):
+        staff_list.append(staffs[i])
+    res = result(True, staff_list, None)
+    return res.convert_to_json()
+
+@staff_api.route('/api/staff/<id>',methods=['Delete'])
+@auth.login_required
+def delete_one_staff(id):
+    found_staff = staff.objects(id=ObjectId(id)).first()
+    if found_staff != None:
+        found_staff.delete()
+
+        staffs = staff.objects()
+        staff_list = []
+        for i in range(staffs.count()):
+            staff_list.append(staffs[i])
+        res = result(True, staff_list, None)
+        return res.convert_to_json()
+    else:
+        res = result(False, None, "ID Error or People Not Found")
+        return res.convert_to_json(), 400
